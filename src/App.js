c3bca785f2ae9eb789edc25ec001a25f92399862
Eka Prasetia <ekaone3033@gmail.com>
2020-11-02 21:19:33 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ReactPlayer url="https://www.youtube.com/watch?v=7AQJf_z7v78" />
+      <br />
+      <ReactPlayer url="https://www.youtube.com/watch?v=CUS47BICv0o" />
+    </>
   );
 }
 
